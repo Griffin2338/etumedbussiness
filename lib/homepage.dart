622d1803e95de4d6ecd5c5ededfebc4bc3f38b9d
Suleyman Poyraz <zaryob.dev@gmail.com>
2021-05-19 13:26:59 +0300

    Added ykaya and abdullah changes.

---
+++ lib/homepage.dart
@@ -0,0 +1,75 @@
+import 'package:etumedbussiness/profile.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  PageController _controller = PageController();
+
+  Map<String, IconData> _bottomNavigatorRoutes = {
+    'Homepage': Icons.home_mini,
+    'Search': Icons.explore,
+    'Profile': Icons.account_circle
+  };
+
+  int _currentPage = 0;
+
+  void _onSearchClick(var item) {}
+
+  //late SearchTabClickController _searchTabClickController;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _body,
+    );
+  }
+
+  Widget get _body => Column(
+        children: [
+          Expanded(
+            child: PageView(
+              physics: NeverScrollableScrollPhysics(),
+              controller: _controller,
+              onPageChanged: (idx) => setState(() => _currentPage = idx),
+              children: [
+                Padding(
+                  padding: EdgeInsets.all(0),
+                  child: ProfilePage(),
+                ),
+                Center(child: Text("page2")),
+                Center(child: Text("page3")),
+              ],
+            ),
+          ),
+          BottomNavigationBar(
+            onTap: (idx) {
+              _controller.animateToPage(
+                idx,
+                duration: Duration(milliseconds: 250),
+                curve: Curves.easeInOut,
+              );
+              setState(() {});
+            },
+            currentIndex: _currentPage,
+            items: _bottomNavigatorRoutes.keys
+                .map<BottomNavigationBarItem>(
+                  (key) => BottomNavigationBarItem(
+                    icon: Icon(_bottomNavigatorRoutes[key]),
+                    label: key,
+                  ),
+                )
+                .toList(),
+          )
+        ],
+      );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,14 @@
-// Copyright 2020 The Chromium Authors. All rights reserved.
+// Copyright 2021 The Etumed Bussiness Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// @dart=2.9
-
+import 'package:etumedbussiness/homepage.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_signin_button/button_builder.dart';
 
-import './register_page.dart';
+//import './register_page.dart';
 import './signin_page.dart';
 
 // Requires that the Firebase Auth emulator is running locally
@@ -28,55 +27,14 @@ class AuthExampleApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Firebase Example App',
+      title: 'Etumed App',
       theme: ThemeData.dark(),
       home: Scaffold(
-        body: AuthTypeSelector(),
+        body: SignIn(),
       ),
-    );
-  }
-}
-
-/// Provides a UI to select a authentication type page
-class AuthTypeSelector extends StatelessWidget {
-  // Navigates to a new page
-  void _pushPage(BuildContext context, Widget page) {
-    Navigator.of(context) /*!*/ .push(
-      MaterialPageRoute<void>(builder: (_) => page),
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Firebase Example App'),
-      ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Container(
-            padding: const EdgeInsets.all(16),
-            alignment: Alignment.center,
-            child: SignInButtonBuilder(
-              icon: Icons.person_add,
-              backgroundColor: Colors.indigo,
-              text: 'Registration',
-              onPressed: () => _pushPage(context, RegisterPage()),
-            ),
-          ),
-          Container(
-            padding: const EdgeInsets.all(16),
-            alignment: Alignment.center,
-            child: SignInButtonBuilder(
-              icon: Icons.verified_user,
-              backgroundColor: Colors.orange,
-              text: 'Sign In',
-              onPressed: () => _pushPage(context, SignInPage()),
-            ),
-          ),
-        ],
-      ),
+      routes: {
+        "/main": (_) => HomePage(),
+      },
     );
   }
 }
